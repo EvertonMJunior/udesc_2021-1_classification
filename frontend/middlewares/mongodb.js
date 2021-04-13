6900b83e3a59d6f46dd67867a8f65e19cfe4f29c
Everton Marcelino Jr <marcelinojr.everton+github@gmail.com>
2021-04-13 10:23:32 -0300

    Fix: Enhanced mongo connection to not open many of them

--- frontend/middlewares/mongodb.js
+++ frontend/middlewares/mongodb.js
@@ -1,17 +1,31 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-const connectDB = (handler) => async (req, res) => {
-  if (mongoose.connections[0].readyState) {
+const options = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  authSource: "admin",
+  useFindAndModify: "false",
+};
+
+// Create cached connection variable
+const connection = {};
+
+const DB_URI = process.env.MONGO_URL;
+
+const connectDb = (handler) => async (req, res) => {
+  if (connection.isConnected) {
     return handler(req, res);
   }
-  await mongoose.connect(process.env.MONGO_URL, {
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-  });
+  try {
+    const dbConnection = await mongoose.connect(DB_URI, options);
+    connection.isConnected = dbConnection.connections[0].readyState;
+  } catch (err) {
+    logger.error(`error connecting to db ${err.message || err}`);
+  }
   return handler(req, res);
 };
 
-export default connectDB;
+export default connectDb;
