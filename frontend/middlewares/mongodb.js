a4fcbc13c7712b349630f798c441d5850fd90633
Everton Marcelino Jr <marcelinojr.everton+github@gmail.com>
2021-04-13 08:15:30 -0300

    Refactor: Change mongo env variable.

--- frontend/middlewares/mongodb.js
+++ frontend/middlewares/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
-  await mongoose.connect(process.env.mongo_url, {
+  await mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
