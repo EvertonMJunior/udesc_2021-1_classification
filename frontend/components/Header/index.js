51c53e36cc6910e213dc8f40a56176f7d3611369
Everton Marcelino Jr <marcelinojr.everton+github@gmail.com>
2021-04-13 11:04:04 -0300

    Feat: Add GA

--- frontend/components/Header/index.js
+++ frontend/components/Header/index.js
@@ -3,6 +3,22 @@ import Head from "next/head";
 const Header = () => (
   <Head>
     <title>UDESC 2021/1 - Classificação extraoficial</title>
+
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-RW16748WEF"
+    ></script>
+
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RW16748WEF');
+        `,
+      }}
+    />
   </Head>
 );
 
